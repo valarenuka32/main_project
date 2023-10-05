fc91a8c4bc62410ca867b985f6a364a31adbd21e
valarenuka32 <132243973+valarenuka32@users.noreply.github.com>
2023-10-05 22:46:51 +0530

    add file

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ const userRouter = require("./user.route");
 const restaurantRouter = require("./restaurant.route");
 const restauranttypeRouter = require("./restaurent_type.route");
 const stateRouter = require("./state.route");
+const paymentRouter = require("./payment.route");
 
 const router = express.Router();
 
@@ -10,5 +11,6 @@ router.use("/user", userRouter);
 router.use("/restaurant", restaurantRouter);
 router.use("/restauranttype", restauranttypeRouter);
 router.use("/state", stateRouter);
+router.use("/payment", paymentRouter);
 
 module.exports = router;
---
+++ src/routes/payment.route.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const { paymentValidation } = require("../validations");
+const { paymentController } = require("../controllers");
+const validate = require("../middlewares/validate");
+
+const router = express.Router();
+
+// create
+router.post(
+    "/create-payment",
+    validate(paymentValidation.createPayment),
+    paymentController.createPayment
+);
+
+// list
+router.get(
+    "/list",
+    paymentController.paymentList
+);
+
+// update
+router.put(
+    "/update-payment:paymentId",
+    paymentController.updateRecode
+);
+
+// delete
+router.delete(
+    "/delete-payment:paymentId",
+    paymentController.deleteRecode
+);
+
+module.exports = router;
