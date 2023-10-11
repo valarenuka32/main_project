b2ba3a36cf993e6abca88f9176b5e3ff96554e7f
valarenuka32 <132243973+valarenuka32@users.noreply.github.com>
2023-10-11 08:14:53 +0530

    add file

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
-const http=require("http");
-const fs=require("fs");
+const http = require("http");
+const fs = require("fs");
+const express = require("express");
+
+const app = express();
+
+const server = http.createServer(app);
+
+server.listen(config.port, () => {
+    console.log(`server start successfully! ${config.port}`);
+});
