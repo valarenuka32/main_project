91010eb2050016bf13fc32f5e7e749527fc32d0c
valarenuka32 <132243973+valarenuka32@users.noreply.github.com>
2023-10-12 08:54:24 +0530

    add file

---
+++ src/models/user.model.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+    {
+        first_name: {
+            type: String,
+            trim: true
+        },
+        last_name: {
+            type: String,
+            trim: true
+        },
+        password: {
+            type: String,
+            trim: true
+        },
+        email: {
+            type: String,
+            trim: true
+        },
+        is_active: {
+            type: Boolean,
+            trim: true,
+        }
+    },
+    {
+        timestamps: true,
+        versionKey: false
+    }
+);
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
